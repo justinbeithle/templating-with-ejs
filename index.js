2efad33b89ca3094376f56312ac3bacf068a8d11
Replit user <>
2023-08-16 02:38:59 +0000

    added hobbies and anime page

--- index.js
+++ index.js
@@ -1,5 +1,58 @@
-var http = require("http");
-http.createServer(function(req,res) {
-  res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.end('Aloha world');
-}).listen(process.env.PORT || 3000);
+const express = require('express');
+const app = express();
+const port = 3000;
+
+var data = require('./data/test.json');
+
+app.set("view engine", "ejs");
+
+//this will allow us to serve up static files, CSS, images & JS
+app.use(express.static(__dirname));
+
+//Home Page 
+app.get('/', (req, res) => {
+  let title = "Home";
+  res.render("pages/index", { "title": title });
+});
+
+//About Page
+app.get('/about', (req, res) => {
+  let title = "About Page";
+  res.render("pages/about", { "title": title });
+});
+
+//Hobbies Page
+app.get('/hobbies', (req, res) => {
+  let title = "Hobbies Page";
+  res.render("pages/hobbies", { "title": title });
+});
+
+//Anime Page
+app.get('/anime', (req, res) => {
+  let title = "Anime Recomendations Page";
+  res.render("pages/anime", { "title": title });
+});
+
+//users index is our list page
+app.get('/users', function(req, res) {
+  var title = 'Users Page';
+  res.render('users/index', {
+    title: title,
+    users: data
+  });
+});
+
+app.get('/users/view/:id', function(req, res) {
+  var title = 'User Page';
+  var id = req.params.id;
+  res.render('users/view', {
+    title: title,
+    user: data[--id]
+  });
+});
+
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+  console.log(data);
+});
